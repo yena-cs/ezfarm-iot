18b88c029992fbbfde102fc2dbf46ba86fc5d066
yena-cs <pynk1210@naver.com>
2021-06-24 01:42:08 +0900

    Update smartfarm2.ino

--- Arduino/smartfarm2/smartfarm2.ino
+++ Arduino/smartfarm2/smartfarm2.ino
@@ -1,23 +1,22 @@
 #include  <DHT.h>  //  온습도 센서를 사용하기 위한 라이브러리
-#include  <SoftwareSerial.h>
 
 #define DHTPIN 3
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
 int CDS_Sensor = A0;
-int Fan_Sensor = 7;
+int prop_SensorA = 5;
+int prop_SensorB = 6;
+int pad_Sensor = 7;
 int light_Sensor = 8;
-int RXPIN = 8;
-int TXPIN = 9;
-SoftwareSerial ESP_wifi(RXPIN, TXPIN);
+
 void setup() {
   Serial.begin(9600);
-  ESP_wifi.begin(9600);
-  ESP_wifi.setTimeout(5000);
   dht.begin();
-  //pinMode(Fan_Sensor, OUTPUT);
-  //pinMode(light_Sensor, OUTPUT);
+  pinMode(prop_SensorA, OUTPUT);
+  pinMode(prop_SensorB, OUTPUT);
+  pinMode(pad_Sensor, OUTPUT);
+  pinMode(light_Sensor, OUTPUT);
 }
 
 void loop() {
@@ -25,23 +24,31 @@ void loop() {
   int t_Sensor = dht.readTemperature();  // 온도
   int CDS_Value = analogRead(CDS_Sensor); // 조도
 
-  if(Serial.available()){
-    ESP_wifi.write(Serial.read());
-  }
-  if(ESP_wifi.available()){
-    Serial.write(ESP_wifi.read());
-  }
-  /*Serial.print("CDS value:"); // 광량과 수치는 반비례
+  Serial.print("CDS value:"); // 광량과 수치는 반비례
   Serial.println(CDS_Value);
   Serial.print("humidity value:");
   Serial.println(h_Sensor);
   Serial.print("temperature value:");
-  Serial.println(t_Sensor);*/
-  /*if(t_Sensor > 25){
-    digitalWrite(Fan_Sensor, HIGH);
+  Serial.println(t_Sensor);
+  digitalWrite(prop_SensorA, LOW);
+  digitalWrite(prop_SensorB, LOW);
+  if(t_Sensor > 30){
+    digitalWrite(prop_SensorA, HIGH);
+    digitalWrite(prop_SensorB, LOW);
   }else{
-    digitalWrite(Fan_Sensor, LOW);
+    digitalWrite(prop_SensorA, LOW);
+    digitalWrite(prop_SensorB, LOW);
   }
-  digitalWrite(light_Sensor, HIGH);*/
+  if(t_Sensor < 25){
+    digitalWrite(pad_Sensor, HIGH);
+  }else{
+    digitalWrite(pad_Sensor, LOW);
+  }
+  if(CDS_Value > 500){
+    digitalWrite(light_Sensor, HIGH);
+  }else{
+    digitalWrite(light_Sensor, LOW);
+  }
   delay(3000);
 }
+ 
